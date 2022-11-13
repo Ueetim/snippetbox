2ff9aa8ee0d3b0236811168cd6eae774d9dca40f
uduak <lorduduak@gmail.com>
2022-11-13 16:52:48 +0100

    added rotes

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+// view snippet
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// create snippet
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
